8eb409563badf6d3e2492492c1f63fcd7766669a
Prasundas99 <prasundas115@gmail.com>
2022-11-02 01:19:46 +0530

    TS variables

--- 01. Intro/02.variables.js
+++ 01. Intro/02.variables.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * variables in TypeScript are similar to variables in JavaScript
  * the main difference is that we can define the type of the variable
@@ -14,16 +15,31 @@
  * - never
  * - object
  */
+exports.__esModule = true;
 // Declaring vaiables in TypeScript with its types
 // 1. String
 var grettings = "Hello World";
 console.log(grettings);
+// grettings = 6; // Error: Type 'number' is not assignable to type 'string'.
+// this is the type safety feature of TypeScript that prevents us from assigning a value of a different type to a variable
+// and more easily find bugs in our code also TS will give some auto completion related to that data type
+grettings.toUpperCase(); // this will give auto completion related to string
+console.log(grettings.toUpperCase());
 // 2. Number
 var age = 30;
 console.log(age);
+age.toPrecision(2); // this will give auto completion related to number
+// number is for numbers like 1, 5.3, -10. JavaScript does not have a special runtime value for integers, so there's no equivalent to the int or float - everything is a number.
 // 3. Boolean
 var isMarried = true;
 console.log(isMarried);
+// Type inference
+// TypeScript can infer the type of a variable based on the value assigned to it
+var name = "John";
+// name = 6; // Error: Type 'number' is not assignable to type 'string'.
+// name = true; // Error: Type 'boolean' is not assignable to type 'string'.
+// name = null; // Error: Type 'null' is not assignable to type 'string'.
+console.log(name);
 // 4. Any
 var anything = "Hello World";
 console.log(anything);
